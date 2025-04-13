5d454bc18808d4406320795ac7f591704ea23aef
estifwonde <estifanoswondwossen396@gmail.com>
2025-04-13 19:51:18 +0300

    intial Stylelint setup

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+export default {
+    extends: "stylelint-config-standard",
+    rules: {
+      "indentation": 2, // Enforce 2 spaces for indentation
+      "color-hex-case": "lower", // Ensure lowercase hex colors
+    },
+  };
+  
