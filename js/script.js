4e6fda1d6e63bd0b32519ea69e4f96052927c6c7
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2025-04-13 21:24:44 +0300

    setup the test

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function doSomething( ) {
+    x = 5
+    var y = 10
+        console.log("Clicked!", x)
+    }
+    
+    var unusedVar = 123
+    
+    function anotherFunc(){
+    console.log(   'This has bad spacing' )
+    }
+    
